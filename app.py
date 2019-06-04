df07a60a308e7580bc5bf2cfbb30437e3eaafd82
leongjinqwen <leongjinqwen@hotmail.com>
2019-06-04 11:57:45 +0800

    add path to wkhtmltopdf

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from flask import Flask, make_response
 
 app = Flask(__name__)
@@ -6,10 +8,23 @@ app = Flask(__name__)
 def index():
     return '<h1>Why so easy</h1>'
 
+def _get_pdfkit_config():
+    """wkhtmltopdf lives and functions differently depending on Windows or Linux. We
+    need to support both since we develop on windows but deploy on Heroku.
+
+    Returns:
+        A pdfkit configuration
+    """
+    if os.environ.get("FLASK_ENV") == "development" :
+        return pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', '/usr/local/bin/wkhtmltopdf'))
+    else:
+        WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip()
+        return pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+
 @app.route("/pdf")
 def show():
     import pdfkit
-    pdf = pdfkit.from_url('http://google.com',False)
+    pdf = pdfkit.from_url('http://google.com',False,configuration=_get_pdfkit_config())
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = f"inline; filename = hello.pdf"
